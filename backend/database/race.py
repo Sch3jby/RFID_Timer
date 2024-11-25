0e5d4c86e29c142e6b43653b5001858f796198aa
Jan_Schejbal <jan.schejbal@tul.cz>
2024-11-25 17:37:15 +0100

    Pozadovany startovni interval se bere z databaze z tabulky zavodu.

--- backend/database/race.py
+++ backend/database/race.py
@@ -6,6 +6,7 @@ class Race(db.Model):
     name = db.Column(db.String(50), nullable=False)
     date = db.Column(db.Date, nullable=False)
     start = db.Column(db.String(1), nullable=False)  # M = mass start, I = interval start
+    interval_time = db.Column(db.Time)
     results_table_name = db.Column(db.String(50))
     description = db.Column(db.String(5000))
 
