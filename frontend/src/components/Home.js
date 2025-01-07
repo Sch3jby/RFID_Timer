8c661b7b8e147d5df7c55aa807bc8c05b9be4c5b
Jan Schejbal <schejbal.jan2@gmail.com>
2025-01-07 09:38:33 +0100

    video na pozadi

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { useTranslation } from '../contexts/LanguageContext';
+import video from '../styles/other/video.mov'
 
 function Home() {
   const { t } = useTranslation();
   return (
     <div className="home-container">
-      <h1>{t('home.welcome')}</h1>
-      <p>{t('home.selectOption')}</p>
+      <div className="video-background">
+        <video src={video} autoPlay muted loop id="background-video"/>
+      </div>
+
+      <div className="content">
+        <h1>{t('home.welcome')}</h1>
+        <p>{t('home.selectOption')}</p>
+      </div>
     </div>
   );
 }
