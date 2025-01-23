b83099e638940ce66f03eca9c06cd3cdd1f57ccd
Jan_Schejbal <jan.schejbal@tul.cz>
2025-01-23 22:11:06 +0100

    Úprava videa na pozadí.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from '../contexts/LanguageContext';
-import video from '../styles/other/video.mov'
+import video from '../styles/other/video.mp4'
 
 function Home() {
   const { t } = useTranslation();
@@ -9,10 +9,10 @@ function Home() {
       <div className="video-background">
         <video src={video} autoPlay muted loop id="background-video"/>
       </div>
-
+      
       <div className="content">
-        <h1>{t('home.welcome')}</h1>
-        <p>{t('home.selectOption')}</p>
+        {/* <h1>{t('home.welcome')}</h1> */}
+        {/* <p>{t('home.selectOption')}</p> */}
       </div>
     </div>
   );
